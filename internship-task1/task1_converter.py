8768a5639cb58d05079ee21368d43be65f7c526b
Mark Solodilin <109875855+msolodilin@users.noreply.github.com>
2023-06-16 12:18:39 +0300

    Remove regex from task1_converter.py

--- internship-task1/task1_converter.py
+++ internship-task1/task1_converter.py
@@ -1,6 +1,5 @@
 import csv
 from datetime import datetime
-import re
 
 filename = 'original1.csv'
 filename_out = 'original1_out.csv'
@@ -8,13 +7,9 @@ filename_out = 'original1_out.csv'
 with open(filename, 'r') as csvfile, open(filename_out, 'w') as csvfile_out:
     datareader = csv.reader(csvfile)
     datawriter = csv.writer(csvfile_out)
+    next(csvfile)
     for row in datareader:
-        regex = r"^(19|20)\d{2}$"
-        if re.match(regex, row[0]):
             ts = datetime.strptime(row[0], "%Y")
             row[0] = ts.strftime('%Y-%m-%d')
             datawriter.writerow(row)
     print(row)
-
-
-#%%
