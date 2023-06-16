6211db334177f2554a797656d277afeefe7b805c
Mark Solodilin <109875855+msolodilin@users.noreply.github.com>
2023-06-16 13:03:41 +0300

    Fixed disappearing headers

--- internship-task1/task1_converter.py
+++ internship-task1/task1_converter.py
@@ -5,11 +5,14 @@ filename = 'original1.csv'
 filename_out = 'original1_out.csv'
 
 with open(filename, 'r') as csvfile, open(filename_out, 'w') as csvfile_out:
-    datareader = csv.reader(csvfile)
-    datawriter = csv.writer(csvfile_out)
-    next(csvfile)
-    for row in datareader:
-            ts = datetime.strptime(row[0], "%Y")
-            row[0] = ts.strftime('%Y-%m-%d')
-            datawriter.writerow(row)
-    print(row)
+    reader = csv.reader(csvfile)
+    writer = csv.writer(csvfile_out)
+    data = list(reader)
+
+    for row in data[1:]:
+        ts = datetime.strptime(row[0], "%Y")
+        row[0] = ts.strftime('%Y-%m-%d')
+
+    writer.writerows(data)
+
+print(data)
